1b0b4bacdd127c03c22efe82aca76df186224757
Marinen87 <Marinen87@gmail.com>
2025-01-11 22:46:39 +0100

    Added Player, Circle classes Added som constant variables Added some code to main.py to implement Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import player
 from constants import *
 
 
@@ -7,8 +8,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    print("Starting asteroids!")
+    player1 = player.Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,7 +19,10 @@ def main():
                 sys.exit()
 
         pygame.Surface.fill(screen, color=(0, 0, 0))
+        player1.update(dt)
+        player1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
