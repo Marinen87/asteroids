f141bcfa8ab2169d57a19ef443c169f6a3b32b43
Marinen87 <Marinen87@gmail.com>
2025-01-11 21:04:38 +0100

    Added main.py Added constants.py Added requirements.txt Added.gitignore file

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+from constants import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("Starting asteroids!")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.Surface.fill(screen, color=(0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
