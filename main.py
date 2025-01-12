e79db789bc5d53353f34e38b325688c2970408f1
Marinen87 <Marinen87@gmail.com>
2025-01-12 16:47:54 +0100

    Added asteroid.py and asteroidfield.py Added logic to spawn and draw asteroids to the screen

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import pygame
 import sys
-import player
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    player1 = player.Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    p1 = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
+    updatable.add(p1)
+    drawable.add(p1)
+
+    p1.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    AsteroidField()
 
     clock = pygame.time.Clock()
     dt = 0
@@ -19,8 +33,11 @@ def main():
                 sys.exit()
 
         pygame.Surface.fill(screen, color=(0, 0, 0))
-        player1.update(dt)
-        player1.draw(screen)
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -4,6 +4,7 @@ from constants import *
 
 
 class Player(circleshape.CircleShape):
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
 
